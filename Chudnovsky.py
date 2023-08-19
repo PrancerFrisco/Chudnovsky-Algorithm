35b2e16f0786ffa57e448c232d96827f150821f7
PrancerFrisco <142399951+PrancerFrisco@users.noreply.github.com>
2023-08-19 21:35:56 +0800

    Chudnovsky.py

--- Chudnovsky.py
+++ Chudnovsky.py
@@ -1,5 +1,4 @@
-from mpmath import mp, mpf
-from math import factorial
+from mpmath import mp, mpf, sqrt
 from time import time
 
 print("Chudnovsky algorithm")
@@ -11,29 +10,27 @@ print("Chudnovsky algorithm")
 # THE FUNCTION
 def chudnovsky(digits):
     mp.dps = digits + 10  # extra precision
-    C = mpf(426880) * (mpf(10005) ** mpf(0.5))
+    C = mpf(426880) * sqrt(10005)
     L = mpf(13591409)
     M = mpf(1)
     X = mpf(1)
     K = mpf(-6)
     a1 = mpf(0)
-    a2 = mpf(0)
+
+    const1 = 545140134
+    const2 = -262537412640768000
     for q in range(digits):
         e = (M * L) / X
+        if a1 + e == a1:  # if the value of a1 is the same as it was before the latest a1 += e. i.e, the value added is so small that it doesn't get computed
+            break
         a1 += e
-        if a1 == a2:  # if the value of a1 is the same as it was before the latest a1 += e. i.e, the value added is so small that it doesn't get computed
-            break  # stops summation
-        a2 += e  # updates the variable a2 AFTER checking if a1 == a2
-        L += mpf(545140134)
-        X *= mpf(-262537412640768000)
+        L += const1
+        X *= const2
         K += 12
-        M *= (mpf(K)**mpf(3) - mpf(16)*mpf(K)) / (mpf(q+1)**mpf(3))
-
+        M *= (K**3 - 16*K) / (mpf(q+1)**3)
 
     print()
-    return str(C * a1 ** -1)[:-10]  # cuts off extra potentially inaccurate digits
-
-
+    return str(C * a1 ** -1)[:digits+1]  # cuts off extra potentially inaccurate digits
 
 
 
@@ -49,10 +46,7 @@ while True: # loops until an integer is entered
             print("Enter an integer")
 
 
-
-
-# PRINT RESULTS
-start_time = time()
-print(BetterChudnovsky(user_input))
+start = time()
+print(chudnovsky(user_input))
 print()
-print(f"Elapsed time: [{time() - start_time}s]") # outputs elapsed time
+print(f"Elapsed time: [{time() - start}s]")
